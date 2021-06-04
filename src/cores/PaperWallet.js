00c7922fc8d0956c54829bcf4ff8dee1f25020ca
wayne.lee <wayne.lee@tideisun.com>
2021-06-04 16:00:29 +0800

    add annotate

--- src/cores/PaperWallet.js
+++ src/cores/PaperWallet.js
@@ -6,18 +6,87 @@ class PaperWallet {
   static EXT_CHAININDEX = 0;
   static EXT_KEYINDEX = 0;
 
+  /**
+   * keyObject{
+   *   address: "008aeeda4d805471df9b2a5b0f38a0c3bcba786b",
+   *   Crypto: {
+   *     cipher: "aes-128-ctr",
+   *     ciphertext: "5318b4d5bcd28de64ee5559e671353e16f075ecae9f99c7a79a38af5f869aa46",
+   *     cipherparams: {
+   *       iv: "6087dab2f9fdbbfaddc31a909735c1e6"
+   *     },
+   *     mac: "517ead924a9d0dc3124507e3393d175ce3ff7c1e96529c6c555ce9e51205e9b2",
+   *     kdf: "pbkdf2",
+   *     kdfparams: {
+   *       c: 262144,
+   *       dklen: 32,
+   *       prf: "hmac-sha256",
+   *       salt: "ae3cd4e7013836a3df6bd7241b12db061dbe2c6785853cce422d148a624ce0bd"
+   *     }
+   *   },
+   *   id: "e13b209c-3b2f-4327-bab0-3bef2e51630d",
+   *   version: 3
+   * }
+   */
+
+  /**
+   * @method createWallet
+   * @param {string} key
+   * @param {string} password
+   * @returns {object} keyObject
+   */
   static createWallet() {}
 
+  /**
+   * @method recoverFromJson
+   * @param {string} content
+   * @param {string} password
+   * @returns {object} keyObject
+   */
   static recoverFromJson() {}
 
+  /**
+   * @method updatePassword
+   * @param {object} oriKeyObject
+   * @param {string} newPassword
+   * @return {object} keyObject
+   */
   static updatePassword() {}
 
+  /**
+   * @method magicSeed
+   * @param {Buffer} pk
+   * @returns {string} pk used keccak256 twice
+   */
   static magicSeed() {}
 
-  static getPubKey(seed) {}
+  /**
+   * @method getPubKey
+   * @param {Buffer} seed bip seed
+   * @param {number} chainIndex - integer for hdwallet chainIndex
+   * @param {number} keyIndex - integer for hdwallet keyIndex
+   * @param {string} [path] - default EXT_PATH
+   * @param {boolean} [compressed] - default true
+   * @returns {string}
+   */
+  static getPubKey(seed, chainIndex, keyIndex, path, compressed) {}
 
-  static getPriKey(seed) {}
+  /**
+   * @method getPriKey
+   * @param {Buffer} seed bip seed
+   * @param {number} chainIndex - integer for hdwallet chainIndex
+   * @param {number} keyIndex - integer for hdwallet keyIndex
+   * @param {string} [path] - default EXT_PATH
+   * @param {boolean} [compressed] - default true
+   * @returns {string}
+   */
+  static getPriKey(seed, chainIndex, keyIndex, path, compressed) {}
 
+  /**
+   * @method getExtendedPublicKey
+   * @param {Buffer} seed 
+   * @returns {string}
+   */
   static getExtendedPublicKey(seed) {
     let root = bitcoin.bip32.fromSeed(seed);
     root = root.derivePath(PaperWallet.EXT_PATH);
@@ -27,9 +96,19 @@ class PaperWallet {
     return xPub;
   }
 
-  static walletToJson() {}
+  /**
+   * @method walletToJson
+   * @param {object} wallet - keyObject
+   * @returns {string} wallet to string
+   */
+  static walletToJson(wallet) {}
 
-  static jsonToWallet() {}
+  /**
+   * @method jsonToWallet
+   * @param {string} walletStr - keyObject string
+   * @returns {object} keyObject
+   */
+  static jsonToWallet(walletStr) {}
 }
 
 module.exports = PaperWallet;
