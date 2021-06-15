3adf7a93d27b0453ed6ae8704ceb99ba18b118e2
wayne.lee <wayne.lee@tideisun.com>
2021-06-15 16:00:26 +0800

    sign do not have privakey input

--- src/cores/Signer.js
+++ src/cores/Signer.js
@@ -1,6 +1,8 @@
 const EthUtils = require('ethereumjs-util');
 const { BN, ecsign } = EthUtils;
 
+const PaperWallet = require('./PaperWallet');
+
 const ZERO32 = Buffer.alloc(32, 0);
 const EC_GROUP_ORDER = Buffer.from('fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141', 'hex');
 
@@ -10,8 +12,12 @@ const THROW_BAD_PRIVATE = 'Expected Private';
 class Signer {
   static instance;
 
-  constructor() {
+  constructor(seed, chainIndex, keyIndex, options = {}) {
     if (!Signer.instance) {
+      this._seed = seed;
+      this._chainIndex = chainIndex;
+      this._keyIndex = keyIndex;
+      this._options = options;
       Signer.instance = this;
     }
 
@@ -36,12 +42,19 @@ class Signer {
         this._compare(x, EC_GROUP_ORDER) < 0; // < G
   }
 
-  sign(hashData, privateKey) {
+  _sign(hashData, privateKey) {
     if(!Buffer.isBuffer(hashData) || !this._isScalar(hashData)) throw new Error(THROW_BAD_HASH);
     if(!Buffer.isBuffer(privateKey) || !this._isPrivate(privateKey)) throw new Error(THROW_BAD_PRIVATE);
 
     const sig = ecsign(hashData, privateKey);
     return sig;
+  }
+
+  sign(hashData) {
+    return this._sign(
+      hashData,
+      Buffer.from(PaperWallet.getPriKey(this._seed, this._chainIndex, this._keyIndex, this._options), 'hex')
+    );
   }
 }
 
--- test/Signer.test.js
+++ test/Signer.test.js
@@ -1,24 +1,44 @@
 const Signer = require('./../src/cores/Signer');
 const Cryptor = require('./../src/helpers/Cryptor');
 
-test('sign', () => {
+const seed = Buffer.from('35f8af7f1bdb4c53446f43c6f22ba0b525634ab556229fffd0f1813cc75b3a2c');
+const signer = new Signer(seed, 0, 0);
+
+test('_sign', () => {
   const rawTransaction = '0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675';
   const privKey = '929cb0a76cccbb93283832c5833d53ce7048c085648eb367a9e63c44c146b35d';
-  // const expR = '2d46c6f11924d035824935697fc64630168ff32b579572862c9cf5b4633d0841';
-  const expR = '20479130526553237106528541717068131205762657723827754116476001462341250910273';
-  // const expS = '53a90b40f98322c4eca2b3eafb3bde0e87749328cbbc3c13cfa783e7fb29885f';
-  const expS = '37840641257281765352258832136473066154395701468149433111297311269542393645151';
+  const expR = '2d46c6f11924d035824935697fc64630168ff32b579572862c9cf5b4633d0841';
+  // == 20479130526553237106528541717068131205762657723827754116476001462341250910273
+  const expS = '53a90b40f98322c4eca2b3eafb3bde0e87749328cbbc3c13cfa783e7fb29885f';
+  // == 37840641257281765352258832136473066154395701468149433111297311269542393645151
   const expV = 28;
 
   const hashData = Cryptor.keccak256round(rawTransaction, 1);
 
-  const signer = new Signer();
-  const signature = signer.sign(Buffer.from(hashData, 'hex'), Buffer.from(privKey, 'hex'));
-  const resR = signer._compare(signature.r, expR);
-  const resS = signer._compare(signature.s, expS);
-  const resV = signer._compare(signature.v, expV);
+  const signature = signer._sign(Buffer.from(hashData, 'hex'), Buffer.from(privKey, 'hex'));
+  const resR = signature.r.toString('hex');
+  const resS = signature.s.toString('hex');
+  const resV = signature.v;
 
-  expect(resR).toBe(0);
-  expect(resS).toBe(0);
-  expect(resV).toBe(0);
+  expect(resR).toBe(expR);
+  expect(resS).toBe(expS);
+  expect(resV).toBe(expV);
+})
+
+test('sign', () => {
+  const rawTransaction = '0xd46e8dd67c5d32be8d46e8dd67c5d32be8058bb8eb970870f072445675058bb8eb970870f072445675';
+  const expR = 'ba18b68bc6a42f9f2fb4774b8dc7766582662566a74f75d70d1438860f03b04f';
+  const expS = '47c782a1c7f51cf78881b1189ce4bcc30a895eb6ffe7b63d66186d36db7407e8';
+  const expV = 28;
+
+  const hashData = Cryptor.keccak256round(rawTransaction, 1);
+
+  const signature = signer.sign(Buffer.from(hashData, 'hex'));
+  const resR = signature.r.toString('hex');
+  const resS = signature.s.toString('hex');
+  const resV = signature.v;
+
+  expect(resR).toBe(expR);
+  expect(resS).toBe(expS);
+  expect(resV).toBe(expV);
 })
