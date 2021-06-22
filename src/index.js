a75a5f75caab3c54c702fbbe1a2999028a67d0d7
Luphia Chang <luphia@mermer.cc>
2021-06-22 16:47:59 +0800

    Update test case

--- src/index.js
+++ src/index.js
@@ -32,8 +32,11 @@ class TideWallet {
 
     const exist = await this.user.checkUser();
     if (!exist) {
-      const seed = this.user.mnemonicToSeed(user.mnemonic, user.password);
-      await this.user.createUserWithSeed(user.OAuthID, seed, user.InstallID);
+      if(user.mnemonic && user.password) {
+        await this.user.createUserWithSeed(user.OAuthID, seed, user.InstallID);
+      } else {
+        await this.user.createUser(user.OAuthID, user.InstallID);
+      }
     }
   
     this.account = new Account(initObj);
@@ -62,13 +65,17 @@ const api = {
   apiKey: 'f2a76e8431b02f263a0e1a0c34a70466',
   apiSecret: '9e37d67450dc906042fde75113ecb78c',
 };
-const user = {
+const user1 = {
   OAuthID: 'test2ejknkjdniednwjq',
   InstallID: '11f6d3e524f367952cb838bf7ef24e0cfb5865d7b8a8fe5c699f748b2fada249',
   mnemonic: 'cry hub inmate cliff sun program public else atom absurd release inherit funny edge assault',
   password: '12345'
 };
-tw.init({ user, api }).then(() => {
+const user2 = {
+  OAuthID: 'test2ejknkjdniednwjq',
+  InstallID: '11f6d3e524f367952cb838bf7ef24e0cfb5865d7b8a8fe5c699f748b2fada249'
+};
+tw.init({ user2, api }).then(() => {
   console.log('TideWallet Ready');
 })
 
