467dce8c3baa1aa359bf96ebf567ad36aafa6c26
Luphia Chang <luphia@mermer.cc>
2021-06-22 16:57:19 +0800

    Add test case in window

--- src/index.js
+++ src/index.js
@@ -56,35 +56,32 @@ class TideWallet {
   }
 }
 
-/**
- * Test in Browser
-
-const tw = new TideWallet();
-const api = {
-  apiURL: 'https://service.tidewallet.io',
-  apiKey: 'f2a76e8431b02f263a0e1a0c34a70466',
-  apiSecret: '9e37d67450dc906042fde75113ecb78c',
-};
-const user1 = {
-  OAuthID: 'test2ejknkjdniednwjq',
-  InstallID: '11f6d3e524f367952cb838bf7ef24e0cfb5865d7b8a8fe5c699f748b2fada249',
-  mnemonic: 'cry hub inmate cliff sun program public else atom absurd release inherit funny edge assault',
-  password: '12345'
-};
-const user2 = {
-  OAuthID: 'test2ejknkjdniednwjq',
-  InstallID: '11f6d3e524f367952cb838bf7ef24e0cfb5865d7b8a8fe5c699f748b2fada249'
-};
-tw.init({ user2, api }).then(() => {
-  console.log('TideWallet Ready');
-})
-
- */
-
-
 if (isBrowser()) {
   window.Buffer = require("buffer").Buffer;
   window.TideWallet = TideWallet;
+
+  /** test case */
+  window.test = () => {
+    const tw = new TideWallet();
+    const api = {
+      apiURL: 'https://service.tidewallet.io',
+      apiKey: 'f2a76e8431b02f263a0e1a0c34a70466',
+      apiSecret: '9e37d67450dc906042fde75113ecb78c',
+    };
+    const user1 = {
+      OAuthID: 'test2ejknkjdniednwjq',
+      InstallID: '11f6d3e524f367952cb838bf7ef24e0cfb5865d7b8a8fe5c699f748b2fada249',
+      mnemonic: 'cry hub inmate cliff sun program public else atom absurd release inherit funny edge assault',
+      password: '12345'
+    };
+    const user2 = {
+      OAuthID: 'test2ejknkjdniednwjq',
+      InstallID: '11f6d3e524f367952cb838bf7ef24e0cfb5865d7b8a8fe5c699f748b2fada249'
+    };
+    tw.init({ user2, api }).then(() => {
+      console.log('TideWallet Ready');
+    });
+  }
 }
 
 module.exports = TideWallet;
