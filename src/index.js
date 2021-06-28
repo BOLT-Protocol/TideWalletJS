bdf8eeb60247a77bb64e46fd003eb438c78c2cc3
wayne.lee <wayne.lee@tideisun.com>
2021-06-28 17:21:54 +0800

    part of api

--- src/index.js
+++ src/index.js
@@ -40,6 +40,9 @@ class TideWallet {
     this.account = new Account(initObj);
     this.account.setMessenger();
     await this.account.init();
+
+    this.trader = new Trader(initObj);
+    this.communicator = communicator; // ++ 確定是否要這樣做
   
     const listener = this.account.messenger.subscribe((v) => {
       this._callback(v, 'update');
@@ -68,7 +71,7 @@ class TideWallet {
 
   async overview() {
     const currencies = await this.account.getAllCurrencies();
-    const fiat = [];
+    const fiat = await this.trader.getFiatList();
     const balance = currencies.reduce((rs, curr) => {
       return rs;
     }, 0);
@@ -80,40 +83,68 @@ class TideWallet {
     return dashboard;
   }
 
-  async getCurrencyDetail({ accountcurrencyId }) {
-
+  /**
+   * 
+   * @param {object} accountInfo
+   * @param {string} accountInfo.assetID
+   */
+  async getAssetDetail({ assetID }) {
+    const asset = await this.account.getCurrencies(assetID);
+    const transactions = await this.account.getTransactions(assetID);
+    
+    return { asset, transactions };
   }
 
-  async getTransactionDetail() {
-
+  // ++ 確定是否要這樣做
+  async getTransactionDetail({ transactionID }) {
+    try {
+      const tx = this.communicator.TransactionDetail(transactionID);
+      return tx;
+    } catch (error) {
+      console.error(error);
+      return error;
+    }
   }
 
-  async getReceivingAddress() {
+  async getReceivingAddress({ accountID }) {
+    const address = await this.account.getReceiveAddress(accountID);
 
+    return address;
   }
 
-  async getTransactionFee() {
+  // ++ need help
+  async getTransactionFee({ accountID, blockchainID, from, to, amount, data }) {
+    const svc = this.account.getService(accountID);
+    const fees = svc.getTransactionFee(blockchainID);
 
+    return fees;
   }
 
+  // need help
   async prepareTransaction() {
 
   }
 
-  async sendTransaction() {
+  async sendTransaction({ accountID, blockchainID, transaction }) {
+      const svc = this.account.getService(accountID);
+      const res = svc.publishTransaction(blockchainID, transaction);
 
+      return res;
   }
 
+  // ++ 確定是否要這樣做
   async sync() {
-
+    await this.account.sync();
   }
 
+  // need help
   async backup() {
 
   }
 
   async close() {
     // release all resources
+    this.account.close();
     return true;
   }
 
@@ -136,7 +167,7 @@ if (isBrowser()) {
   window.test = async() => {
     const tw = new TideWallet();
     const api = {
-      apiURL: 'https://service.tidewallet.io/api/v1',
+      apiURL: 'https://staging.tidewallet.io/api/v1',
       apiKey: 'f2a76e8431b02f263a0e1a0c34a70466',
       apiSecret: '9e37d67450dc906042fde75113ecb78c',
     };
@@ -151,6 +182,11 @@ if (isBrowser()) {
       InstallID: '11f6d3e524f367952cb838bf7ef24e0cfb5865d7b8a8fe5c699f748b2fada249'
     };
     await tw.init({ user: user2, api });
+    //test
+    console.log('overview:', await tw.overview());
+    // console.log('getAssetDetail:', await tw.getAssetDetail({ assetID: "a7255d05-eacf-4278-9139-0cfceb9abed6" }));
+    console.log('getReceivingAddress:', await tw.getReceivingAddress({ accountID: "a7255d05-eacf-4278-9139-0cfceb9abed6" }));
+
   }
 }
 
