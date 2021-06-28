0cfdbaec237b913d7f386dd67c88c26203a0dd21
wayne.lee <wayne.lee@tideisun.com>
2021-06-28 11:31:20 +0800

    fix user

--- src/cores/User.js
+++ src/cores/User.js
@@ -292,7 +292,7 @@ class User {
           })
         await this._communicator.login(_token, _tokenSecret);
       } catch (e) {
-        const res = await this._communicator.register(this.installId, this.installId, await this._PaperWallet.getExtendedPublicKey());
+        const res = await this._communicator.register(this.installId, this.installId, await this._TideWalletCore.getExtendedPublicKey());
 
         if (res.token) {
           _token = res.token
