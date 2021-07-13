956267c50885d4fa32c89feab3917279b57a9b68
wayne.lee <wayne.lee@tideisun.com>
2021-07-13 11:38:06 +0800

    rename and add some function

---
+++ src/helpers/utils.js
@@ -0,0 +1,74 @@
+const randomHex = (n) => {
+  var ID = "";
+  var text = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  n = parseInt(n);
+  if (!(n > 0)) {
+    n = 8;
+  }
+  while (ID.length < n) {
+    ID = ID.concat(text.charAt(parseInt(Math.random() * text.length)));
+  }
+  return ID;
+};
+
+function isUint(value, bit) {
+  return (value >= 0 && value <= pow(2, bit) - 1);
+}
+
+/**
+ * @param {Buffer} x 
+ * @returns 
+ */
+function toDER(x) {
+  let input = Buffer.from([...x]);
+  const ZERO = Buffer.alloc(1, 0);
+  let i = 0;
+  while (input[i] == 0) ++i;
+  if (i == input.length) return ZERO;
+  input = input.slice(i);
+  const combine = [...ZERO, ...input];
+  if (x[0] & 0x80 != 0) return Buffer.from(combine);
+  return input;
+}
+
+/**
+ * 
+ * @param {Buffer} r 
+ * @param {Buffer} s 
+ * @returns 
+ */
+function bip66encode(r, s) {
+  const lenR = r.length;
+  const lenS = s.length;
+  if (lenR == 0) throw new Error('R length is zero');
+  if (lenS == 0) throw new Error('S length is zero');
+  if (lenR > 33) throw new Error('R length is too long');
+  if (lenS > 33) throw new Error('S length is too long');
+  if (r[0] & 0x80 != 0) throw new Error('R value is negative');
+  if (s[0] & 0x80 != 0) throw new Error('S value is negative');
+  if (lenR > 1 && (r[0] == 0x00) && r[1] & 0x80 == 0)
+    throw new Error('R value excessively padded');
+  if (lenS > 1 && (s[0] == 0x00) && s[1] & 0x80 == 0)
+    throw new Error('S value excessively padded');
+
+  var signature = Buffer.alloc(6 + lenR + lenS);
+
+  // 0x30 [total-length] 0x02 [R-length] [R] 0x02 [S-length] [S]
+  signature[0] = 0x30;
+  signature[1] = signature.length - 2;
+  signature[2] = 0x02;
+  signature[3] = r.length;
+  signature.copy(r, 4);
+  signature[4 + lenR] = 0x02;
+  signature[5 + lenR] = s.length;
+  signature.copy(s, 5 + lenR);
+  return signature;
+}
+
+
+module.exports = {
+  randomHex,
+  isUint,
+  toDER,
+  bip66encode,
+};
